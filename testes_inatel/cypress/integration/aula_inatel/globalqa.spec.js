77484085acf3bb8872776324aa3e9d27d8b7f7cd
Gabriel Pivoto <gabriel-pivoto@outlook.com>
2021-10-22 16:57:42 -0300

    Primeiro teste do Cypress

---
+++ testes_inatel/cypress/integration/aula_inatel/globalqa.spec.js
@@ -0,0 +1,9 @@
+/// <reference types = "cypress"/>
+
+
+describe('Suíte de teste', () => {
+
+    cy.visit('https://globalsqa.com/angularJs-protractor/registration-login-example/#/login');
+    
+
+})
